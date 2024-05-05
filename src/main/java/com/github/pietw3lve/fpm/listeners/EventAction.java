939a14230cf55b6b8fa9a8695a570d003528c32f
PieTw3lve <ericlim000@outlook.com>
2024-05-04 22:24:22 -0400

    Updated old javadoc

--- src/main/java/com/github/pietw3lve/fpm/listeners/EventAction.java
+++ src/main/java/com/github/pietw3lve/fpm/listeners/EventAction.java
@@ -3,24 +3,25 @@ package com.github.pietw3lve.fpm.listeners;
 import org.bukkit.event.Event;
 
 /**
- * The PlayerAction interface represents an action that can be performed by a player in Minecraft.
- * Implementations of this interface should provide methods to check if the action matches a specific event
- * and to execute the action when the event occurs.
+ * The EventAction interface represents an action that can be performed on an event.
+ * It provides methods to check if the action matches a specific event and to execute the action on the event.
+ *
+ * @param <T> the type of event that this action can handle
  * @author PieTw3lve
  */
 public interface EventAction<T extends Event> {
     /**
-     * Checks if the action matches the given PlayerInteractEvent.
+     * Checks if the action matches the given event.
      *
-     * @param event The PlayerInteractEvent to check against.
-     * @return true if the action matches the event, false otherwise.
+     * @param event the event to check against
+     * @return true if the action matches the event, false otherwise
      */
     boolean matches(T event);
 
     /**
-     * Executes the action when the given PlayerInteractEvent occurs.
+     * Executes the action on the given event.
      *
-     * @param event The PlayerInteractEvent to execute the action for.
+     * @param event the event to execute the action on
      */
     void execute(T event);
 }
