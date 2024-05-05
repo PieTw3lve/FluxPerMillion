38f99c4258d4bb919bc8b2239bc66bf067a141ca
PieTw3lve <ericlim000@outlook.com>
2024-05-04 20:55:54 -0400

    Added the ability to minecart boost with coal in the offhand slot

--- src/main/java/com/github/pietw3lve/fpm/listeners/player/MinecartBoostAction.java
+++ src/main/java/com/github/pietw3lve/fpm/listeners/player/MinecartBoostAction.java
@@ -11,6 +11,7 @@ import org.bukkit.entity.Minecart;
 import org.bukkit.entity.Player;
 import org.bukkit.event.block.Action;
 import org.bukkit.event.player.PlayerInteractEvent;
+import org.bukkit.inventory.ItemStack;
 import org.bukkit.util.Vector;
 
 import com.github.pietw3lve.fpm.FluxPerMillion;
@@ -38,14 +39,16 @@ public class MinecartBoostAction implements EventAction<PlayerInteractEvent> {
         Player player = event.getPlayer();
         Action action = event.getAction();
         Material mainHandItemType = player.getInventory().getItemInMainHand().getType();
-        return !isPlayerOnBoostCooldown(player) && isPlayerInMinecart(player) && isRightClick(action) && isBoostFuel(mainHandItemType);
+        Material offHandItemType = player.getInventory().getItemInOffHand().getType();
+        return !isPlayerOnBoostCooldown(player) && isPlayerInMinecart(player) && isRightClick(action) && (isBoostFuel(mainHandItemType) || isBoostFuel(offHandItemType));
     }
 
     @Override
     public void execute(PlayerInteractEvent event) {
         Player player = event.getPlayer();
-        double points = plugin.getConfig().getDouble(FLUX_POINTS_MINECART_BOOST, DEFAULT_FLUX_POINTS_MINECART_BOOST);
-        long cooldown = plugin.getConfig().getInt(MINECART_BOOST_COOLDOWN, DEFAULT_MINECART_BOOST_COOLDOWN);
+        ItemStack mainHandItem = player.getInventory().getItemInMainHand();
+        ItemStack offHandItem = player.getInventory().getItemInOffHand();
+        
         Minecart minecart = (Minecart) player.getVehicle();
         Vector currentVelocity = minecart.getVelocity();
         Vector boostVelocity = calculateBoostVelocity(player);
@@ -57,14 +60,22 @@ public class MinecartBoostAction implements EventAction<PlayerInteractEvent> {
         player.spawnParticle(org.bukkit.Particle.FLAME, player.getLocation(), 10, 0.5, 0.5, 0.5, 0.1);
         player.playSound(player, org.bukkit.Sound.ENTITY_BLAZE_SHOOT, 1, 1);
         
-        if (player.getGameMode() != GameMode.CREATIVE) player.getInventory().getItemInMainHand().setAmount(player.getInventory().getItemInMainHand().getAmount() - 1);
+        if (player.getGameMode() != GameMode.CREATIVE) {
+            if (isBoostFuel(mainHandItem.getType())) {
+                mainHandItem.setAmount(mainHandItem.getAmount() - 1);
+            } else if (isBoostFuel(offHandItem.getType())) {
+                offHandItem.setAmount(offHandItem.getAmount() - 1);
+            }
+        }
         
         minecartBoostCooldown.add(player.getUniqueId().toString());
         
+        long cooldown = plugin.getConfig().getInt(MINECART_BOOST_COOLDOWN, DEFAULT_MINECART_BOOST_COOLDOWN);
         plugin.getServer().getScheduler().runTaskLater(plugin, () -> {
             minecartBoostCooldown.remove(player.getUniqueId().toString());
         }, cooldown);
 
+        double points = plugin.getConfig().getDouble(FLUX_POINTS_MINECART_BOOST, DEFAULT_FLUX_POINTS_MINECART_BOOST);
         FluxLevelChangeEvent fluxEvent = new FluxLevelChangeEvent(plugin.getFluxMeter(), player.getLocation(), player, "boosted", "minecart", points);
         plugin.getServer().getPluginManager().callEvent(fluxEvent);
     }
