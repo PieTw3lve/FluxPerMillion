1af7055d06e86373e30c5b61852c6d934cc17c27
PieTw3lve <ericlim000@outlook.com>
2025-01-09 16:35:45 -0500

    Flux from blocks burning and grass spreading will now credit players

--- src/main/java/com/github/pietw3lve/fpm/listeners/block/BlockBurnAction.java
+++ src/main/java/com/github/pietw3lve/fpm/listeners/block/BlockBurnAction.java
@@ -1,15 +1,22 @@
 package com.github.pietw3lve.fpm.listeners.block;
 
+import java.util.Collection;
+
+import org.bukkit.Location;
+import org.bukkit.entity.Entity;
+import org.bukkit.entity.Player;
 import org.bukkit.event.block.BlockBurnEvent;
 
 import com.github.pietw3lve.fpm.FluxPerMillion;
 import com.github.pietw3lve.fpm.events.FluxLevelChangeEvent;
 import com.github.pietw3lve.fpm.utils.EventActionUtil;
+import com.github.pietw3lve.fpm.utils.PlayerUtil;
 import com.github.pietw3lve.fpm.utils.SQLiteUtil.ActionCategory;
 
 public class BlockBurnAction implements EventActionUtil<BlockBurnEvent> {
     
     private static final String FLUX_POINTS_BLOCK_BURN = "flux_points.block_burn";
+    private static final int SEARCH_RADIUS = 96;
 
     private final FluxPerMillion plugin;
 
@@ -24,8 +31,13 @@ public class BlockBurnAction implements EventActionUtil<BlockBurnEvent> {
 
     @Override
     public void execute(BlockBurnEvent event) {
+        Location blockLocation = event.getBlock().getLocation();
+        Collection<Entity> nearbyEntities = event.getBlock().getWorld().getNearbyEntities(blockLocation, SEARCH_RADIUS, SEARCH_RADIUS, SEARCH_RADIUS);
+
+        Player closestPlayer = PlayerUtil.findClosestPlayer(blockLocation, nearbyEntities);
+
         double points = plugin.getConfig().getDouble(FLUX_POINTS_BLOCK_BURN);
-        FluxLevelChangeEvent fluxEvent = new FluxLevelChangeEvent(plugin.getFluxMeter(), event.getBlock().getLocation(), null, null, "burned", "block", points, ActionCategory.ENERGY);
+        FluxLevelChangeEvent fluxEvent = new FluxLevelChangeEvent(plugin.getFluxMeter(), blockLocation, closestPlayer, null, "burned", "block", points, ActionCategory.ENERGY);
         plugin.getServer().getPluginManager().callEvent(fluxEvent);
     }
 }
--- src/main/java/com/github/pietw3lve/fpm/listeners/block/GrassSpreadAction.java
+++ src/main/java/com/github/pietw3lve/fpm/listeners/block/GrassSpreadAction.java
@@ -1,17 +1,24 @@
 package com.github.pietw3lve.fpm.listeners.block;
 
+import java.util.Collection;
+
+import org.bukkit.Location;
 import org.bukkit.Material;
 import org.bukkit.block.Block;
+import org.bukkit.entity.Entity;
+import org.bukkit.entity.Player;
 import org.bukkit.event.block.BlockSpreadEvent;
 
 import com.github.pietw3lve.fpm.FluxPerMillion;
 import com.github.pietw3lve.fpm.events.FluxLevelChangeEvent;
 import com.github.pietw3lve.fpm.utils.EventActionUtil;
+import com.github.pietw3lve.fpm.utils.PlayerUtil;
 import com.github.pietw3lve.fpm.utils.SQLiteUtil.ActionCategory;
 
 public class GrassSpreadAction implements EventActionUtil<BlockSpreadEvent> {
     
     private static final String FLUX_POINTS_GRASS_GROWTH = "flux_points.grass_growth";
+    private static final int SEARCH_RADIUS = 96;
 
     private final FluxPerMillion plugin;
 
@@ -27,9 +34,13 @@ public class GrassSpreadAction implements EventActionUtil<BlockSpreadEvent> {
 
     @Override
     public void execute(BlockSpreadEvent event) {
-        Block source = event.getSource();
+        Location blockLocation = event.getSource().getLocation();
+        Collection<Entity> nearbyEntities = event.getBlock().getWorld().getNearbyEntities(blockLocation, SEARCH_RADIUS, SEARCH_RADIUS, SEARCH_RADIUS);
+
+        Player closestPlayer = PlayerUtil.findClosestPlayer(blockLocation, nearbyEntities);
+
         double points = plugin.getConfig().getDouble(FLUX_POINTS_GRASS_GROWTH);
-        FluxLevelChangeEvent fluxEvent = new FluxLevelChangeEvent(plugin.getFluxMeter(), source.getLocation(), null, null, "grown", "grass", points, ActionCategory.AGRICULTURE);
+        FluxLevelChangeEvent fluxEvent = new FluxLevelChangeEvent(plugin.getFluxMeter(), blockLocation, closestPlayer, null, "grown", "grass", points, ActionCategory.AGRICULTURE);
         plugin.getServer().getPluginManager().callEvent(fluxEvent);
     }
 
