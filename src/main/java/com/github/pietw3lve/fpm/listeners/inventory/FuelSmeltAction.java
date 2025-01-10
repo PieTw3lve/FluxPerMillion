aae61f765f123a68475098ed24d2394a8be2a44b
PieTw3lve <ericlim000@outlook.com>
2025-01-09 20:30:41 -0500

    Added multipliers for certain materials

--- src/main/java/com/github/pietw3lve/fpm/listeners/inventory/FuelSmeltAction.java
+++ src/main/java/com/github/pietw3lve/fpm/listeners/inventory/FuelSmeltAction.java
@@ -4,6 +4,7 @@ import java.util.Collection;
 import java.math.BigDecimal;
 import java.math.RoundingMode;
 
+import org.bukkit.Material;
 import org.bukkit.block.Block;
 import org.bukkit.entity.Player;
 import org.bukkit.event.inventory.FurnaceBurnEvent;
@@ -33,7 +34,8 @@ public class FuelSmeltAction implements EventActionUtil<FurnaceBurnEvent> {
         FluxLevelChangeEvent fluxEvent = new FluxLevelChangeEvent();
         Block furnace = event.getBlock();
 
-        double points = plugin.getConfig().getDouble(FLUX_POINTS_FUEL_BURN) * (event.getBurnTime() / 200.0);
+        double multiplier = getMultiplier(event.getFuel().getType());
+        double points = plugin.getConfig().getDouble(FLUX_POINTS_FUEL_BURN) * (event.getBurnTime() / 200.0) * multiplier;
         
         Collection<Player> players = furnace.getChunk().getPlayersSeeingChunk();
         if (!players.isEmpty()) {
@@ -46,6 +48,93 @@ public class FuelSmeltAction implements EventActionUtil<FurnaceBurnEvent> {
         } else {
             fluxEvent = new FluxLevelChangeEvent(plugin.getFluxMeter(), furnace.getLocation(), null, null, "burned", "fuel", points, ActionCategory.ENERGY);
             plugin.getServer().getPluginManager().callEvent(fluxEvent);
+        }
+    }
+
+    private double getMultiplier(Material material) {
+        switch (material) {
+            case BLAZE_ROD:
+                return 0.1;
+            case LAVA_BUCKET:
+                return 0.4;
+            case BAMBOO:
+                return 0.8;
+            case DRIED_KELP_BLOCK:
+                return 0.8;
+            case OAK_LOG:
+                return 0.8;
+            case STRIPPED_OAK_LOG:
+                return 0.8;
+            case OAK_WOOD:
+                return 0.8;
+            case OAK_SAPLING:
+                return 0.8;
+            case SPRUCE_LOG:
+                return 0.8;
+            case STRIPPED_SPRUCE_LOG:
+                return 0.8;
+            case SPRUCE_WOOD:
+                return 0.8;
+            case SPRUCE_SAPLING:
+                return 0.8;
+            case BIRCH_LOG:
+                return 0.8;
+            case STRIPPED_BIRCH_LOG:
+                return 0.8;
+            case BIRCH_WOOD:
+                return 0.8;
+            case BIRCH_SAPLING:
+                return 0.8;
+            case JUNGLE_LOG:
+                return 0.8;
+            case STRIPPED_JUNGLE_LOG:
+                return 0.8;
+            case JUNGLE_WOOD:
+                return 0.8;
+            case JUNGLE_SAPLING:
+                return 0.8;
+            case ACACIA_LOG:
+                return 0.8;
+            case STRIPPED_ACACIA_LOG:
+                return 0.8;
+            case ACACIA_WOOD:
+                return 0.8;
+            case ACACIA_SAPLING:
+                return 0.8;
+            case DARK_OAK_LOG:
+                return 0.8;
+            case STRIPPED_DARK_OAK_LOG:
+                return 0.8;
+            case DARK_OAK_WOOD:
+                return 0.8;
+            case DARK_OAK_SAPLING:
+                return 0.8;
+            case CHERRY_LOG:
+                return 0.8;
+            case STRIPPED_CHERRY_LOG:
+                return 0.8;
+            case CHERRY_WOOD:
+                return 0.8;
+            case CHERRY_SAPLING:
+                return 0.8;
+            case MANGROVE_LOG:
+                return 0.8;
+            case STRIPPED_MANGROVE_LOG:
+                return 0.8;
+            case MANGROVE_WOOD:
+                return 0.8;
+            case MANGROVE_ROOTS:
+                return 0.8;
+            case MANGROVE_PROPAGULE:
+                return 0.8;
+            case CHARCOAL:
+                return 0.8;
+            case COAL:
+                return 2.5;
+            case COAL_BLOCK:
+                return 2.5;
+            default:
+                return 1.0;
         }
     }
 }
